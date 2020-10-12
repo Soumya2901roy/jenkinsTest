6a5a1ac2e209dae7e570d4bc9769babd9f03c951
Soumya2901roy <59948831+Soumya2901roy@users.noreply.github.com>
2020-10-12 08:22:38 +0530

    Create helloworld.py

---
+++ helloworld.py
@@ -0,0 +1,12 @@
+from datetime import datetime
+
+print("HI! this code this being run by Jenkins")
+
+# datetime object containing current date and time
+now = datetime.now()
+ 
+print("now =", now)
+
+# dd/mm/YY H:M:S
+dt_string = now.strftime("%d/%m/%Y %H:%M:%S")
+print("date and time =", dt_string)
